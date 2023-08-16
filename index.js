c1329e28ad7217bf614d01e9370d41d6d88ee539
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-16 18:02:42 +0600

    almost completed

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 document.getElementById('btn').addEventListener('click',function(){
     const coupon = document.getElementById('input');
-    const couponCode =coupon.value;
-    if(couponCode === '30%'){
-    const discountString = .3 ;
+    const couponString =coupon.value
+    const couponCode =parseFloat(couponString)
+    if(couponString.length===0){
+        const heading =document.getElementById('heading')
+        heading.innerText='আপনার কুপনটি জমা দিন' 
+    }
+    else if(couponCode === 30){
+    const discountString =.3;
     const discount =parseFloat(discountString)
     const takaString = document.getElementById('taka').innerText;
     const taka = parseFloat(takaString);
     const discountPrice = taka*discount;
-    const newPrice = taka -discountPrice;
+    const newPrice = taka - discountPrice;
     document.getElementById('offerprice').innerText=newPrice;
     }
     else{
-        document.getElementById('offerprice').innerText=taka;
+        const heading =document.getElementById('heading')
+        heading.innerText='আপনার জন্য কোনো ডিসকাউন্ট নেই'        
     }
 })
